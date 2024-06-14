ae0b71514a181a2fe694a63d387316a7e69a7056
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:51:48 -0400

    Update child_pugh_score.py

--- calculator_implementations/child_pugh_score.py
+++ calculator_implementations/child_pugh_score.py
@@ -1,61 +1,4 @@
-import os
-import json 
 import unit_converter_new
-
-def compute_child_pugh_score(input_variables):
- 
-    inr = input_variables['inr']
-
-    ascites_state = input_variables.get('ascites', 'Absent')
-    encephalopahty_state = input_variables.get('encephalopathy', 'No Encephalopathy')
-
-    bilirubin = unit_converter_new.conversions(input_variables['bilirubin'][0], input_variables['bilirubin'][1], "mg/dL", 584.66, None)
-    albumin = unit_converter_new.conversions(input_variables['albumin'][0], input_variables['albumin'][1], "g/dL", 66500, None)
-
-    bilirubin_score = 0
-    albumin_score = 0
-    inr_score = 0
-    ascites_score = 0
-    enc_score = 0
-    
-    if bilirubin < 2: 
-        bilirubin_score += 1
-    elif 2 < bilirubin < 3:
-        bilirubin_score += 2
-    elif bilirubin > 3:
-        bilirubin_score += 3
-
-    if albumin > 3.5: 
-        albumin_score += 1
-    elif 2.8 < albumin < 3.5:
-        albumin_score += 2
-    elif albumin < 2.8:
-        albumin_score += 3
-
-    if inr < 1.7: 
-        inr_score += 1
-    elif 1.7 <= inr <= 2.3:
-        inr_score += 2
-    elif inr > 2.3:
-        inr_score += 3
-
-    if ascites_state == 'Absent':
-        ascites_score += 1 
-    elif ascites_state == 'Slight':
-        ascites_score += 2
-    elif ascites_state == 'Moderate':
-        ascites_score += 3
-    
-
-    if encephalopahty_state == 'No Encephalopathy':
-        enc_score += 1 
-    elif encephalopahty_state == 'Grade 1-2':
-        enc_score += 2
-    elif encephalopahty_state== 'Grade 3-4':
-        enc_score += 3
-
-    return bilirubin_score + albumin_score + inr_score + ascites_score + enc_score
-
 
 def compute_child_pugh_score_explanation(input_variables):
 
@@ -143,53 +86,6 @@ def compute_child_pugh_score_explanation(input_variables):
 
     explanation += f"The patient's child pugh score is {cp_score}.\n"
 
-    return {"Explanation": explanation, "Answer": cp_score, "Calculator Answer": compute_child_pugh_score(input_variables)}
+    return {"Explanation": explanation, "Answer": cp_score}
 
 
-test_outputs = [{"albumin": [3.7, "g/dL"],
-                 "bilirubin": [2, "mg/dL"], 
-                 'inr': 2,
-                 'ascites': "Slight", 
-                 'encephalopathy': 'Grade 1-2'}, 
-
-                {"albumin": [3, "g/dL"],
-                 "bilirubin": [2.5, "mg/dL"], 
-                 'inr': 1.5,
-                 'ascites': "Absent", 
-                 'encephalopathy': 'No Encephalopathy'}, 
-
-                {"albumin": [2.5, "g/dL"],
-                 "bilirubin": [1.9, "mg/dL"], 
-                 'inr': 3.6,
-                 'ascites': "Moderate", 
-                 'encephalopathy': 'Grade 3-4'}, 
-                
-                {"albumin": [2.5, "g/dL"],
-                 "bilirubin": [2.5, "mg/dL"], 
-                 'inr': 1.0}]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = compute_child_pugh_score_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/child_pugh.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-
-file_name = "explanations/child_pugh.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
-
-
-    
