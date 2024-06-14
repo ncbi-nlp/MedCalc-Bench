beebdf1e008b68fea3f4238d06a0d6b24f1892e7
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:36:21 -0400

    Update sOsm.py

--- calculator_implementations/sOsm.py
+++ calculator_implementations/sOsm.py
@@ -1,17 +1,5 @@
-import os
-import json
 import unit_converter_new
 from rounding import round_number
-
-
-def compute_serum_osmolality(input_parameters):
-    
-    sodium = unit_converter_new.conversions(input_parameters["sodium"][0], input_parameters["sodium"][1], "mmol/L", 22.99, 1)
-    bun = unit_converter_new.conversions(input_parameters["bun"][0], input_parameters["bun"][1], "mg/dL", 28.02, None)
-    glucose = unit_converter_new.conversions(input_parameters["glucose"][0], input_parameters["glucose"][1], "mg/dL", 180.16, None)
-
-    return 2 * sodium + (bun / 2.8) + (glucose / 18)
-
 
 
 def compute_serum_osmolality_explanation(input_parameters):
@@ -31,19 +19,4 @@ def compute_serum_osmolality_explanation(input_parameters):
     explanation += f"Plugging these values into the equation, we get 2 * {sodium} + ({bun} / 2.8) + ({bun} / 18) = {serum_os} mmol/L."
     explanation += f"The patient's calculated serum osmolality concentration is {serum_os} mmol/L. This is equalivalent to {serum_os} mOsm/kg.\n"
 
-    return {"Explanation": explanation, "Answer": serum_os, "Calculator Answer": compute_serum_osmolality(input_parameters)}
-
-
-test_outputs = [{"sodium": [140, "mEq/L"], "bun": [14.01, "mg/dL"], "glucose": [80, "mg/dL"]}, 
-              {"sodium": [140.0, "mmol/L"], "bun": [5.00, "mmol/L"], "glucose": [4.44, "mmol/L"]}
-              ]
-
-outputs = ""
-for i, test_case in enumerate(test_outputs):
-    outputs += "Explanation:" + "\n"+  compute_serum_osmolality_explanation(test_case)["Explanation"] + "\n"
-
-
-file_name = "explanations/sOsm.txt"
-
-with open(file_name, 'w') as file:
-    file.write(outputs)
+    return {"Explanation": explanation, "Answer": serum_os}
