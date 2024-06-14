2ff67f05f7dec7ba6f43e6b6779d761cc9d2d0ad
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:35:29 -0400

    Update sodium_correction_hyperglycemia.py

--- calculator_implementations/sodium_correction_hyperglycemia.py
+++ calculator_implementations/sodium_correction_hyperglycemia.py
@@ -1,19 +1,5 @@
-import os
-import json
 import unit_converter_new
 from rounding import round_number
-
-
-def compute_sodium_correction_hyperglycemia(input_variables):
-
-    sodium = input_variables["sodium"]
-    glucose = input_variables["glucose"]
-
-    sodium = unit_converter_new.conversions(sodium[0], sodium[1], "mEq/L", 22.99, 1)
-    glucose = unit_converter_new.conversions(glucose[0], glucose[1], "mg/dL", 180.16, None)
-
-    return sodium + 0.024 * (glucose - 100)
-
 
 def compute_sodium_correction_hyperglycemia_explanation(input_variables):
 
@@ -34,24 +20,4 @@ def compute_sodium_correction_hyperglycemia_explanation(input_variables):
 
     explanation += f"Hence, the patient's corrected concentration of sodium is {corrected_sodium} mEq/L.\n"
 
-    return {"Explanation": explanation, "Answer": corrected_sodium, "Calculator Answer": compute_sodium_correction_hyperglycemia(input_variables)}
-
-
-
-test_outputs = [
-    {"sodium": [140.0, "mEq/L"], "glucose": [4.44, "mmol/L"]}, 
-    {"sodium": [140.0, "mmol/L"], "glucose": [80.00, "mg/dL"]} 
-      
-]
-
-outputs = ""
-for i, test_case in enumerate(test_outputs):
-    outputs += "Explanation:\n" +  compute_sodium_correction_hyperglycemia_explanation(test_case)["Explanation"] + "\n"
-
-
-file_name = "explanations/sodium_correction_hyperglycemia.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-
-with open(file_name, 'w') as file:
-    file.write(outputs)
+    return {"Explanation": explanation, "Answer": corrected_sodium}
