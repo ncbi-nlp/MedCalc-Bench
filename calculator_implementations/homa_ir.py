1dd7865fe98473c4f28ee0e2b4b5ee9521c297b9
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:44:11 -0400

    Update homa_ir.py

--- calculator_implementations/homa_ir.py
+++ calculator_implementations/homa_ir.py
@@ -1,20 +1,6 @@
-import json
-import os
 import unit_converter_new
 from rounding import round_number
 
-
-def compute_homa_ir(input_variables):
-
-    if input_variables["insulin"][1] == "pmol/L":
-        input_variables["insulin"][0] =  input_variables["insulin"][0] * 6
-    elif input_variables["insulin"][1] == "ng/mL":
-        input_variables["insulin"][0] = input_variables["insulin"][0] * 24.8
-
-    #insulin = unit_converter_new.conversions(input_variables["insulin"][0], input_variables["insulin"][1], "µIU/mL", 5734, None )
-    glucose = unit_converter_new.conversions(input_variables["glucose"][0], input_variables["glucose"][1], "mg/dL", 180.16, None)
-
-    return (input_variables["insulin"][0] * glucose)/405
 
 
 def compute_homa_ir_explanation(input_variables):
@@ -43,41 +29,6 @@ def compute_homa_ir_explanation(input_variables):
 
     explanation += f"Plugging into the formula will give us {insulin} * {glucose}/405 = {answer}. Hence, the patient's HOMA-IR score is {answer}. \n"
 
-    return {"Explanation": explanation, "Answer": answer, "Calculator Answer": compute_homa_ir(input_variables)}
+    return {"Explanation": explanation, "Answer": answer}
 
-
-output = {}
-
-test_outputs = [
-    {
-        'insulin': [12.43, "µIU/mL"],
-        'glucose': [85.23, "mg/dL"]
-    },
-    {
-        'insulin': [74.58, "pmol/L"],
-        'glucose': [4.73, "mmol/dL"]
-    }
-]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = compute_homa_ir_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/homa_ir.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-file_name = "explanations/homa_ir.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
 
