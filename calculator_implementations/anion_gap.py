238f5161fb92d8360f6bf931ff90172f7c849425
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:48:09 -0400

    Update anion_gap.py

--- calculator_implementations/anion_gap.py
+++ calculator_implementations/anion_gap.py
@@ -1,21 +1,5 @@
-import json
-import os
 import unit_converter_new
 from rounding import round_number
-
-
-def compute_anion_gap(input_parameters):
-
-    sodium = input_parameters["sodium"]
-    chloride = input_parameters["chloride"]
-    bicarbonate = input_parameters["bicarbonate"]
-
-    sodium = unit_converter_new.conversions(sodium[0], sodium[1], "mEq/L", 22.99, 1)
-    chloride = unit_converter_new.conversions(chloride[0], chloride[1], "mEq/L", 35.45, 1)
-    bicarbonate = unit_converter_new.conversions(bicarbonate[0], bicarbonate[1], "mEq/L", 61.02, 1)
-
-    return sodium - (chloride + bicarbonate)
-
 
 def compute_anion_gap_explanation(input_parameters):
 
@@ -40,31 +24,6 @@ def compute_anion_gap_explanation(input_parameters):
     explanation += f"Plugging in these values into the anion gap formula gives us {sodium} mEq/L - ({chloride} mEq/L + {bicarbonate} mEq/L) = {answer} mEq/L. "
     explanation += f"Hence, The patient's anion gap is {answer} mEq/L.\n"
 
-    return {"Explanation": explanation, "Answer": answer, "Calculator Answer": compute_anion_gap(input_parameters)}
+    return {"Explanation": explanation, "Answer": answer}
 
-test_cases = [{"sodium": [140.02, "mmol/L"], "chloride": [100.02, "mmol/L"], "bicarbonate": [28.02, "mmol/L"]},
-             {"sodium": [112.03, "mmol/L"], "chloride": [90.02, "mmol/L"], "bicarbonate": [28.02, "mmol/L"]}]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_cases):
-    outputs[i] = compute_anion_gap_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/anion_gap.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-
-file_name = "explanations/anion_gap.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
 
