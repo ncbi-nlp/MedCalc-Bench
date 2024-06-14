c35fbd000a3a02d079a3f2fabfb9f0fe99b3d869
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:36:48 -0400

    Update qt_calculator_rautaharju.py

--- calculator_implementations/qt_calculator_rautaharju.py
+++ calculator_implementations/qt_calculator_rautaharju.py
@@ -2,16 +2,6 @@ import os
 import json 
 from rounding import round_number
 
-def rautaharju_calculator(input_variables):
-    heart_rate = input_variables["heart_rate"][0]
-    qt_interval_ms = input_variables["qt_interval"][0]
-
-    qt_interval = qt_interval_ms
-
-    qt_c = qt_interval * (120 + heart_rate) / 180
-
-    return qt_c
-
 def rautaharju_calculator_explanation(input_variables):
     heart_rate = input_variables["heart_rate"][0]
     qt_interval = input_variables["qt_interval"][0]
@@ -26,33 +16,4 @@ def rautaharju_calculator_explanation(input_variables):
     explanation += f"Hence, plugging in these values, we will get {qt_interval} x (120 + {heart_rate}) / 180 = {qt_c}.\n"
     explanation += f"The patient's corrected QT interval (QTc) is {qt_c} msec.\n"
 
-    return {"Explanation": explanation, "Answer": qt_c, "Calculator Answer": rautaharju_calculator(input_variables)}
-
-
-test_outputs = [
-    {"heart_rate": [75, "beats per minute"], "qt_interval": [400, "msec"]},  
-    {"heart_rate": [60, "beats per minute"], "qt_interval": [350, "msec"]},  
-    {"heart_rate": [90, "beats per minute"], "qt_interval": [450, "msec"]}, 
-]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = rautaharju_calculator_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/qt_calculator_rautaharju.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-file_name = "explanations/qt_calculator_rautaharju.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
+    return {"Explanation": explanation, "Answer": qt_c}
