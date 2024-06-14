55ce1d27f4237b471cd73865d4127b5f61a4568d
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:49:44 -0400

    Update calcium_correction.py

--- calculator_implementations/calcium_correction.py
+++ calculator_implementations/calcium_correction.py
@@ -1,25 +1,5 @@
-import os
-import json
 import unit_converter_new
 from rounding import round_number
-
-def calculate_corrected_calcium(params):
-
-    normal_albumin = 4.0 # Normal albumin level in g/dL
-    albumin = params.get('albumin')
-    albumin_val = albumin[0]
-    albumin_units = albumin[1]
-    calcium = params.get('calcium')
-    calcium_val = calcium[0]
-    calcium_units = calcium[1]
-
-    albumin_val = unit_converter_new.conversions(albumin_val, albumin_units, "g/dL", 66500, None)
-    calcium_val = unit_converter_new.conversions(calcium_val, calcium_units, "mg/dL", 40.08, 2)
-
-    corrected_calcium = (0.8 * (normal_albumin - albumin_val)) + calcium_val
-
-    return corrected_calcium
-
 
 def calculate_corrected_calcium_explanation(params):
 
@@ -53,40 +33,6 @@ def calculate_corrected_calcium_explanation(params):
 
     output += f"The patient's corrected calcium concentration {corrected_calcium} mg/dL.\n"
 
-    return {"Explanation": output, "Answer": corrected_calcium, "Calculator Answer": calculate_corrected_calcium(params)}
-
-
-output = {}
-
-
-test_outputs = [{"albumin": [3.00, "g/dL"], 
-                 "calcium": [10.0, "mg/dL"]}, 
-
-                 {"albumin": [30.0, "g/L"], 
-                 "calcium": [2.50, "mmol/L"]}
-                ]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = calculate_corrected_calcium_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/calcium_correction.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-
-file_name = "explanations/calcium_correction.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
+    return {"Explanation": output, "Answer": corrected_calcium}
 
    
