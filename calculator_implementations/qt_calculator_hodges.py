9bb0e105eb9fbe211f6035bb12e392508506d7d5
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:37:06 -0400

    Update qt_calculator_hodges.py

--- calculator_implementations/qt_calculator_hodges.py
+++ calculator_implementations/qt_calculator_hodges.py
@@ -2,18 +2,6 @@ import os
 import json 
 from rounding import round_number
 
-def hodges_calculator(input_variables):
-    heart_rate = input_variables["heart_rate"][0]
-    qt_interval_ms = input_variables["qt_interval"][0]
-
-    qt_interval = qt_interval_ms
-
-    rr_interval_sec =  60 / heart_rate
-
-    qt_c = qt_interval + 1.75 * ((60 /rr_interval_sec) - 60)
-
-    return qt_c
-
 def hodges_calculator_explanation(input_variables):
     heart_rate = input_variables["heart_rate"][0]
     qt_interval = input_variables["qt_interval"][0]
@@ -31,34 +19,4 @@ def hodges_calculator_explanation(input_variables):
 
     explanation += f"The patient's corrected QT interval (QTc) is {qt_c} msec.\n"
 
-    return {"Explanation": explanation, "Answer": qt_c, "Calculator Answer": hodges_calculator(input_variables)}
-
-
-test_outputs = [
-    {"heart_rate": [75, "beats per minute"], "qt_interval": [400, "msec"]},  
-    {"heart_rate": [60, "beats per minute"], "qt_interval": [350, "msec"]},  
-    {"heart_rate": [90, "beats per minute"], "qt_interval": [450, "msec"]}, 
-]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = hodges_calculator_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/qt_calculator_hodges.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-
-file_name = "explanations/qt_calculator_hodges.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
+    return {"Explanation": explanation, "Answer": qt_c}
