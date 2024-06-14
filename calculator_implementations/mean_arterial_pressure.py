18032700af827b9c822ece3ae9ec6e5ebcf6f12f
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:41:34 -0400

    Update mean_arterial_pressure.py

--- calculator_implementations/mean_arterial_pressure.py
+++ calculator_implementations/mean_arterial_pressure.py
@@ -1,14 +1,4 @@
-import json
-import os
 from rounding import round_number
-
-
-def calculate_map(variables):
-    sys_bp = variables['sys_bp'][0]
-    dia_bp = variables['dia_bp'][0]
-
-
-    return 2*dia_bp/3 + sys_bp/3
 
 def mean_arterial_pressure_explanation(variables):
 
@@ -23,28 +13,3 @@ def mean_arterial_pressure_explanation(variables):
     output += f"Hence, the patient's mean arterial pressure is {value} mm Hg.\n"
 
     return {"Explanation": output, "Answer": value, "Calculator Answer": calculate_map(variables)}
-
-'''
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] =  mean_arterial_pressure_explanation(test_outputs[i])
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-
-file_name = "explanations/mean_arterial_pressure.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-
-
-
-file_name = "explanations/mean_arterial_pressure.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
-'''
