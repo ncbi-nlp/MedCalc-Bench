11d952622b05e81e388a751507857b3a8677064e
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:33:38 -0400

    Update steroid_conversion_calculator.py

--- calculator_implementations/steroid_conversion_calculator.py
+++ calculator_implementations/steroid_conversion_calculator.py
@@ -3,34 +3,6 @@ import json
 import unit_converter_new
 from rounding import round_number
 
-
-def compute_steroid_conversion(input_parameters):
-
-    conversion_dict = {"Betamethasone IV": 1, 
-                    "Cortisone PO": 33.333, 
-                    "Dexamethasone IV": 1, 
-                    "Dexamethasone PO": 1,
-                    "Hydrocortisone IV": 26.667,
-                    "Hydrocortisone PO": 26.667,
-                    "MethylPrednisoLONE IV": 5.333,
-                    "MethylPrednisoLONE PO": 5.333,
-                    "PrednisoLONE PO": 6.667, 
-                    "PredniSONE PO": 6.667, 
-                    "Triamcinolone IV": 5.333
-                }
-    
-    input_drug_mass = unit_converter_new.conversions(input_parameters["input steroid"][1], input_parameters["input steroid"][2], "mg", None, None)
-    
-    target_drug_name = input_parameters["target steroid"]
-    input_drug_name = input_parameters["input steroid"][0]
-
-
-    from_multiplier = conversion_dict[input_drug_name]
-    to_multiplier = conversion_dict[target_drug_name]
-    conversion_factor = (to_multiplier / from_multiplier)
-    converted_amount = input_drug_mass * (conversion_factor)
-    return converted_amount
-
 def compute_steroid_conversion_explanation(input_parameters):
 
 
@@ -71,18 +43,5 @@ def compute_steroid_conversion_explanation(input_parameters):
     explanation += f"{input_drug_mass} {input_unit} of {input_drug_name} is equal to {converted_amount} mg of {target_drug_name}.\n"
     
 
-    return {"Explanation": explanation, "Answer": converted_amount, "Calculator Answer": compute_steroid_conversion(input_parameters)}
+    return {"Explanation": explanation, "Answer": converted_amount}
 
-
-test_cases = [{"input steroid": ["PredniSONE PO", 2, "mg"], "target steroid": "PrednisoLONE PO"}, {"input steroid": ["PredniSONE PO", 0.1, "g"], "target steroid": "Hydrocortisone IV"}]
-
-outputs = ""
-
-for i in range(len(test_cases)):
-    outputs += "Explanations:\n" + compute_steroid_conversion_explanation(test_cases[i])["Explanation"] + "\n"
-
-
-file_name = "explanations/steroid_conversion_explanation.txt"
-
-with open(file_name, 'w') as file:
-    file.write(outputs)
