26dfa2bf64d2c8b7f4049e265bcc41581a973598
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:01:59 -0400

    Update adjusted_body_weight.py

--- calculator_implementations/adjusted_body_weight.py
+++ calculator_implementations/adjusted_body_weight.py
@@ -4,18 +4,6 @@ import ideal_body_weight
 import os
 from rounding import round_number
 
-def abw_calculator(input_variables):
-
-    gender = input_variables["sex"]
-
-    weight = weight_conversion.weight_conversion(input_variables["weight"])
-
-    ibw = ideal_body_weight.ibw_calculator({"sex": gender, "height": input_variables["height"]})
-
-    abw = ibw + 0.4 * (weight - ibw)
-
-    return abw
-
 
 def abw_explanation(input_variables):
 
@@ -39,31 +27,3 @@ def abw_explanation(input_variables):
 
     return {"Explanation": explanation, "ABW": abw_explanation_string, "Answer": abw, "Calculator Answer": abw_calculator(input_variables)}
 
-
-outputs = {}
-
-test_outputs = [ {"weight": [123, "lbs"],  "height": [5, "ft", 2.5, "in"], "sex": "Male"}, {"weight": [40.3 , "kg"], "height": [120.3, "cm"], "sex": "Female"},
-{"weight": [54.1, "kg"] , "height": [73.2, "in"], "sex": "Female"}]
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = abw_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/adjusted_body_weight.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-
-file_name = "explanations/adjusted_body_weight.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
