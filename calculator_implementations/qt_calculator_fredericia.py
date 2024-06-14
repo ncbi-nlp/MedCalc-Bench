85faff850ebbef0496b598aa00e603d0ddb0c720
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-06-14 15:37:44 -0400

    Update qt_calculator_fredericia.py

--- calculator_implementations/qt_calculator_fredericia.py
+++ calculator_implementations/qt_calculator_fredericia.py
@@ -1,19 +1,4 @@
-import os
-import json 
 from rounding import round_number
-
-
-def fredericia_calculator(input_variables):
-    heart_rate = input_variables["heart_rate"][0]
-    qt_interval_ms = input_variables["qt_interval"][0]
-
-    qt_interval_sec = qt_interval_ms
-
-    rr_interval_sec = 60 / heart_rate
-
-    qt_c = qt_interval_sec / (rr_interval_sec) ** (1/3)
-
-    return qt_c
 
 def fredericia_calculator_explanation(input_variables):
     heart_rate = input_variables["heart_rate"][0]
@@ -32,35 +17,6 @@ def fredericia_calculator_explanation(input_variables):
 
     explanation += f"The patient's corrected QT interval (QTc) is {qt_c} msec. "
 
-    return {"Explanation": explanation, "Answer": qt_c, "Calculator Answer": fredericia_calculator(input_variables)}
+    return {"Explanation": explanation, "Answer": qt_c}
 
 
-
-test_outputs = [
-    {"heart_rate": [75, "beats per minute"], "qt_interval": [400, "msec"]},  
-    {"heart_rate": [60, "beats per minute"], "qt_interval": [350, "msec"]},  
-    {"heart_rate": [90, "beats per minute"], "qt_interval": [450, "msec"]}, 
-]
-
-
-outputs = {}
-explanations = ""
-for i, test_case in enumerate(test_outputs):
-    outputs[i] = fredericia_calculator_explanation(test_case)
-    explanations += "Explanation:\n"
-    explanations += outputs[i]["Explanation"]
-    explanations += "\n"
-
-'''
-file_name = "explanations/qt_calculator_fredericia.json"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    json.dump(outputs, file, indent=4)
-'''
-
-file_name = "explanations/qt_calculator_fredericia.txt"
-os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-with open(file_name, 'w') as file:
-    file.write(explanations)
