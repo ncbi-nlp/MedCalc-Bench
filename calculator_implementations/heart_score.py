23e8f8ecef54a8b5d4c290cf010db4d9eb80f19f
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-11-15 12:56:23 -0500

    Update heart_score.py

--- calculator_implementations/heart_score.py
+++ calculator_implementations/heart_score.py
@@ -3,6 +3,19 @@ import age_conversion
             
 def compute_heart_score_explanation(input_parameters):
 
+   explanation = """
+   The HEART Score for risk stratification in patients with chest pain is shown below:
+
+   1. History: Slightly suspicious = 0 points, Moderately suspicious = +1 point, Highly suspicious = +2 points
+   2. EKG: Normal = 0 points, Non-specific repolarization disturbance = +1 point, Significant ST deviation = +2 points
+   3. Age: <45 years = 0 points, 45-64 years = +1 point, ≥65 years = +2 points
+   4. Risk factors (HTN, hypercholesterolemia, DM, obesity (BMI >30 kg/m²), smoking (current or cessation within 3 months), positive family history of cardiovascular disease before age 65, atherosclerotic disease such as prior MI, PCI/CABG, CVA/TIA, or peripheral arterial disease): No known risk factors = 0 points, 1-2 risk factors = +1 point, ≥3 risk factors or history of atherosclerotic disease = +2 points
+   5. Initial troponin level: ≤normal limit = 0 points, 1–3× normal limit = +1 point, >3× normal limit = +2 points
+
+   The total score is calculated by summing the points for each criterion.\n\n
+  """
+
+
     # Define parameters and their default values
     parameters = {
         'history': {'Slightly suspicious': 0, 'Moderately suspicious': 1, 'Highly suspicious': 2},
@@ -21,7 +34,7 @@ def compute_heart_score_explanation(input_parameters):
 
     # Initialize total score and output explanation
     total_score = 0
-    explanation = "The current HEART Score is 0.\n"
+    explanation += "The current HEART Score is 0.\n"
 
     for param, options in parameters.items():
         param_value = input_parameters.get(param)
