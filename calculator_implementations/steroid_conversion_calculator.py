b1381c3bad8721dcae35f89cb992f88b13544913
Nikhil Khandekar <nskhandekar7153@gmail.com>
2024-11-15 15:58:40 -0500

    Update steroid_conversion_calculator.py

--- calculator_implementations/steroid_conversion_calculator.py
+++ calculator_implementations/steroid_conversion_calculator.py
@@ -4,6 +4,20 @@ from rounding import round_number
 def compute_steroid_conversion_explanation(input_parameters):
 
 
+   explanation = """
+    The Steroid Conversions providing equivalent doses for various corticosteroids are listed below:
+    
+       1. Betamethasone: Route = IV, Equivalent Dose = 0.75 mg
+       2. Cortisone: Route = PO, Equivalent Dose = 25 mg
+       3. Dexamethasone (Decadron): Route = IV or PO, Equivalent Dose = 0.75 mg
+       4. Hydrocortisone: Route = IV or PO, Equivalent Dose = 20 mg
+       5. MethylPrednisoLONE: Route = IV or PO, Equivalent Dose = 4 mg
+       6. PrednisoLONE: Route = PO, Equivalent Dose = 5 mg
+       7. PredniSONE: Route = PO, Equivalent Dose = 5 mg
+       8. Triamcinolone: Route = IV, Equivalent Dose = 4 mg 
+    """
+
+
     conversion_dict = {"Betamethasone IV": 1, 
                     "Cortisone PO": 33.33, 
                     "Dexamethasone IV": 1, 
@@ -17,7 +31,7 @@ def compute_steroid_conversion_explanation(input_parameters):
                     "Triamcinolone IV": 5.33
                 }
     
-    explanation = ""
+    explanation += "\n\n"
     input_drug_mass_exp, input_drug_mass = unit_converter_new.conversion_explanation(input_parameters["input steroid"][1], input_parameters["input steroid"][0], None, None, input_parameters["input steroid"][2], "mg")
     explanation += input_drug_mass_exp
 
